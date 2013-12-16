991675162f2edde72a302a81f3f46f4d13f516ea
Vaughan Schmidt <vaughan.schmidt@gmail.com>
2013-12-16 15:33:55 -0500

    Added hw1_q4 (Hailstone problem)

---
+++ hw1_q4.py
@@ -0,0 +1,30 @@
+"""Hailstone Sequence problem"""
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its length.
+
+    >>> a = hailstone(10)  # Seven elements are 10, 5, 16, 8, 4, 2, 1
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    assert n > 0, 'Seed number %d is not greater than 0' % n    
+    count = 1
+    while n != 1:
+        print(n)
+        if(n % 2 == 0):  # N is even
+            n = n // 2
+        else:        # N is odd
+            n = (n * 3) + 1
+        count += 1
+    print(n)
+    return count
+
+        
+        
